a60018d8080b9a947c6c7ee69ce8e22661b5ed6a
Leonardo Nunes <leonardonmattoslopes@hotmail.com>
2024-08-19 14:04:04 -0300

    Implementando Logs

---
+++ WebApi8CadastroDeLivros/Logs/Log.cs
@@ -0,0 +1,36 @@
+﻿namespace WebApi8CadastroDeLivros.Logs
+{
+    public static class Log
+    {
+
+        public static void LogToFile(string title, string logMessage)
+        {
+
+            string fileName = DateTime.Now.ToString("ddMMyyyy") + ".txt";
+
+            StreamWriter swLog;
+
+            if (File.Exists(fileName))
+            {
+                swLog = File.AppendText(fileName);
+            }
+            else 
+            {
+                swLog = new StreamWriter(fileName);      
+            }
+
+            swLog.WriteLine("Log: ");
+            swLog.WriteLine(DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString());
+
+            swLog.WriteLine("Titulo da Mensagem: " + title);
+            swLog.WriteLine("Mensagem : " + logMessage);
+
+            swLog.WriteLine("---------------------------------");
+            swLog.WriteLine("");
+            swLog.Close();
+
+        }
+
+
+    }
+}
